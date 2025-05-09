71b98a17bd99c1d04e403692323284ebfbb951a0
Kolitha Rathnayake <kolitharathnayake229@gmail.com>
2025-05-09 22:35:33 +0530

    update item part js

--- finez furniture/model/ItemModel.js
+++
@@ -1,9 +0,0 @@
-export  default class itemModel{
-    constructor(itemCode,itemName,itemPrice,itemQuantity) {
-        this.itemCode=itemCode;
-        this.itemName=itemName;
-        this.itemPrice=itemPrice;
-        this.itemQuantity=itemQuantity;
-
-    }
-}
