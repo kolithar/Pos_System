6b07ba298a2d949ead9baad0dfc0a0178f326a1d
Kolitha Rathnayake <kolitharathnayake229@gmail.com>
2025-05-10 06:42:18 +0530

    update item part js

--- finez furniture/control/ItemController.js
+++ finez furniture/control/ItemController.js
@@ -1,6 +1,13 @@
-export default class Customer{
-
-}
 
 var form = document.getElementById("myForm"),
     imgInput = document.querySelector(".img"),
+    name = document.getElementById("name"),
+    bread = document.getElementById("bread"),
+    date = document.getElementById("date"),
+    woodmaterial = document.getElementById("wood-material"),
+    othermaterial = document.getElementById("other-material"),
+    price = document.getElementById("price"),
+    materialcolor = document.getElementById("material-color"),
+    Itemnot = document.getElementById("Item-not"),
+    submitBtn = document.querySelector(".submit")
+     userInfo = document.getElementById("data")
